03a7dc849d59e050c739034fe389e03484f00b60
Jean-Louis <jeanlouis.ung@epita.fr>
2021-02-03 14:53:10 +0100

    TDD: add unit test -> test_return_0

--- test_calculator.py
+++ test_calculator.py
@@ -6,3 +6,8 @@ from calculator import add
 def test_throw_error_if_not_number():
     with pytest.raises(TypeError):
         add('1', '2')
+
+
+def test_return_0():
+    res = add(0, 0)
+    assert res == 0
